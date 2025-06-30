49995e9f855ac6d4f47fcdb12933f6f3387dd2ff
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-06-30 15:12:43 +0700

    update

--- index.js
+++ index.js
@@ -4,52 +4,45 @@ const tarif = {
   Lansia: 30_000,
 };
 
-const umur = 29;
-const jumlahTiket = 5;
+// main function
+function beliTiket(umur, jumlahTiket) {
+  const riwayatPembelian = [];
 
-const riwayatPembelian = [];
+  // 1. tentukan kategori & harga tiket berdasarkan umur
+  const kategori = kategoriBerdasarkanUmur(umur);
 
-// 1. tentukan kategori & harga tiket berdasarkan umur
-const kategori = kategoriBerdasarkanUmur(umur);
+  // 2. bikin object dengan format: {kategori: 'Anak - anak', jumlah: 2, totalHarga: 50_000}
+  const totalHarga = hitungTotalHarga(kategori, jumlahTiket);
 
-// 2. bikin object dengan format: {kategori: 'Anak - anak', jumlah: 2, totalHarga: 50_000}
-const totalHarga = hitungTotalHarga(kategori, jumlahTiket);
+  const tiket = {
+    kategori: kategori,
+    jumlah: jumlahTiket,
+    totalHarga: totalHarga,
+  };
 
-const tiket = {
-  kategori: kategori,
-  jumlah: jumlahTiket,
-  totalHarga: totalHarga,
-};
+  // 3. masukkan object ke array riwayatPembelian
+  riwayatPembelian.push(tiket);
 
-// 3. masukkan object ke array riwayatPembelian
-riwayatPembelian.push(tiket);
+  // 4. Tampilkan riwayat pembelian
+  console.table(riwayatPembelian);
+}
 
-// 4. Tampilkan riwayat pembelian
-console.table(riwayatPembelian);
-
-// expected output
-// [{ kategori: "Anak - anak", jumlah: 2, totalHarga: 50_000 }];
-
+// sub function
 function kategoriBerdasarkanUmur(umur) {
-  // let kategori = "";
-
-  // if (umur < 12) {
-  //   kategori = "Anak - anak";
-  // } else if (umur >= 12 && umur < 60) {
-  //   kategori = "Dewasa";
-  // } else {
-  //   kategori = "Lansia";
-  // }
-
-  // return kategori;
-
   if (umur < 12) return "Anak - anak";
   if (umur < 60) return "Dewasa";
   return "Lansia";
 }
 
+// sub function
 function hitungTotalHarga(kategori, jumlahTiket) {
   const harga = tarif[kategori] * jumlahTiket;
   const ppn = harga * 0.11;
   return harga + ppn;
 }
+
+// input
+const umur = 29;
+const jumlahTiket = 5;
+
+beliTiket(umur, jumlahTiket);
